ef1a730df4e2b9b6d4a95eba40960696261f168e
Rob McDaniel <robmcdan@gmail.com>
2015-03-28 15:28:04 -0700

    initial commit of collateral

---
+++ best.r
@@ -0,0 +1,6 @@
+best <- function(state, outcome) {
+    ## Read outcome data
+    ## Check that state and outcome are valid
+    ## Return hospital name in that state with lowest 30-day death
+    ## rate
+}
